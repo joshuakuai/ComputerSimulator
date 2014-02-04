bb39d82cd25da8f9837b43c45fd02989f7b5a698
iampolo <yeungsangsin@gmail.com>
2014-02-04 13:31:31 -0800

    display IR as binary format only

--- src/main/edu/gwu/cs6461/logic/Register.java
+++ src/main/edu/gwu/cs6461/logic/Register.java
@@ -38,8 +38,11 @@ public class Register extends Observable{
 		data = newData;
 		
 		HardwareData hardwareData = new HardwareData();
-		//hardwareData.put(this.name, Integer.toBinaryString(0x100000 | newData).substring(1));
-		hardwareData.put(this.name, Integer.toString(newData));
+		if ("IR".equals(this.name)) {
+			hardwareData.put(this.name, Integer.toBinaryString(0x100000 | newData).substring(1));
+		} else 
+			hardwareData.put(this.name, Integer.toString(newData));
+		
 		this.notifyObservers(hardwareData);
 		
 		CPUController.shareInstance().checkSingleStepModel();
